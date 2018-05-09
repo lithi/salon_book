c18cf89ad27968fcc3b7a162ad3476a493567917
Taha <nasrolahi@gmail.com>
2018-05-09 21:48:29 +0430

    Cloned Abarcloud docker files

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
 # Use the `ENTRYPOINT` DeploymentConfig environment variable to specify
 # which command to run. This enables the same Dockerfile to be used for
 # web and worker processes. This script contains the basics for a standard
 # Rails app with sidekiq workers but it can be customized to add other
 # entrypoints.
+#!/usr/bin/env bash
 
 # Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
 if [ "$ENTRYPOINT" = "workers" ]
